7287d7b5b78943774427810a92d8c182b7582336
rodolfolopes <rodolfolopes1996@hotmail.com>
2022-03-10 12:32:56 -0300

    Exercicio 1 Feito

---
+++ Unidade01/src/SomaDoisNumeros/SomaDoisNumeros.java
@@ -0,0 +1,23 @@
+package SomaDoisNumeros;
+import java.util.*;
+
+class SomaDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		//Declaração de variáveis
+		int num1 , num2 , soma;
+		
+		//Leituras
+		System.out.println("Digite um número:");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número:");
+		num2 = sc.nextInt();
+		
+		//Somar
+		soma = num1 + num2;
+		
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);
+	}
+}
